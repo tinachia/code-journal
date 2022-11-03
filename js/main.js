6e4fd6e454bb9a0359da545bdee7473c44414600
Tina Chia <tinachia_5@yahoo.com>
2022-11-03 06:17:44 +0000

    view entries - create dom tree

--- js/main.js
+++ js/main.js
@@ -23,3 +23,44 @@ function submitForm(event) {
   $form.reset();
   $updateImg.setAttribute('src', 'images/placeholder-image-square.jpg');
 }
+
+// DOM Tree//
+
+// function newEntries(entries) {
+//   var $list = document.createElement('li');
+//   $list.setAttribute('class', 'column-full');
+
+//   var $divRow = document.createElement('div');
+//   $divRow.setAttribute('div', 'row');
+
+//   $list.appendChild($divRow);
+
+//   var $divColHalf1 = document.createElement('div');
+//   $divColHalf1.setAttribute('class', 'column-half');
+
+//   $divRow.appendChild($divColHalf1);
+
+//   var $img = document.createElement('img');
+//   $img.setAttribute('src', 'images/placeholder-image-square.jpg');
+//   $img.setAttribute('alt', 'placeholder');
+
+//   $divColHalf1.appendChild($img);
+//   $divColHalf1.appendChild($img);
+
+//   var $divColHalf2 = document.createElement('div');
+//   $divColHalf2.setAttribute('class', 'column-half');
+
+//   $divRow.appendChild($divColHalf2);
+
+//   var $title = document.createElement('h2');
+//   $title.setAttribute('class', 'entry-title');
+
+//   $divColHalf2.appendChild($title);
+
+//   var $note = document.createElement('p');
+//   $note.setAttribute('class', 'entry-note');
+
+//   $divColHalf2.appendChild($note);
+
+//   return $list;
+// }
